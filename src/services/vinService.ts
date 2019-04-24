458bb5299e339ab9f4d26397e93dbc51207b1158
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-04-24 12:14:51 -0300

    improve coding style

--- src/services/vinService.ts
+++ src/services/vinService.ts
@@ -10,14 +10,14 @@ export const filter = (vin: string) =>
 export const validate = (vin: string): string => vin.length !== 17 && "17 chars expected"
 
 export const convert = (res: VinCheckResponse): CarInfo => {
-    const Results = res && res.Results
-    return Results && Results.length > 0
+    const results = res && res.Results
+    return results && results.length > 0
         ? {
-              make: findValue(res.Results, "Make"),
-              model: findValue(res.Results, "Model"),
-              year: parseInt(findValue(res.Results, "Model Year")),
-              trim: findValue(res.Results, "Trim"),
-              vehicleType: findValue(res.Results, "Vehicle Type")
+              make: findValue(results, "Make"),
+              model: findValue(results, "Model"),
+              year: parseInt(findValue(results, "Model Year"), 10),
+              trim: findValue(results, "Trim"),
+              vehicleType: findValue(results, "Vehicle Type")
           }
         : null
 }
@@ -26,6 +26,6 @@ export const apiCheck = async (vin: string): Promise<CarInfo> =>
     convert(await get<VinCheckResponse>("https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/" + vin, { format: "json" }))
 
 const findValue = (results: VinResultEntry[], variable: string): string => {
-    const result = results.find(result => result.Variable === variable)
-    return result ? result.Value : null
+    const item = results.find(result => result.Variable === variable)
+    return item ? item.Value : null
 }
